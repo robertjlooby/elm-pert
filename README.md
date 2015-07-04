7cbb69ca4cfdb6e2d2c19e9d82cb11f6470870a1
Rob Looby <robertjlooby@gmail.com>
2015-07-03 19:41:10 -0500

    switch to browser test runner

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Tests
 
-`elm-test test/AllTests.elm`
+1. Run `elm reactor`
+2. Navigate browser to [http://localhost:8000/test/AllTests.elm?debug](http://localhost:8000/test/AllTests.elm?debug)1. Run `elm reactor`
--- test/AllTests.elm
+++ test/AllTests.elm
@@ -1,9 +1,7 @@
 module Main where
 
-import ElmTest.Runner.Console exposing (runDisplay)
+import ElmTest.Runner.Element exposing (runDisplay)
 import ElmTest.Test exposing (Test, suite)
-import IO.IO exposing (..)
-import IO.Runner exposing (Request, Response)
 
 import PertTest
 
@@ -12,10 +10,4 @@ tests = suite "All elm-pert specs" [
                 PertTest.tests
         ]
 
-console : IO ()
-console = runDisplay tests
-
-port requests : Signal Request
-port requests = IO.Runner.run responses console
-
-port responses : Signal Response
+main = runDisplay tests
